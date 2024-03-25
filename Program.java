1277c88e14a5eb224dae337866c37de0a2e1a5ec
luishf2014 <luishgamer2237@gmail.com>
2024-03-25 16:59:34 -0300

    Ex02

--- Program.java
+++ Program.java
@@ -1,7 +1,9 @@
 public class Program {
     public static void main(String[] args) {
-        Ex01 ex01 = new Ex01();
-        ex01.ex01();
-
+        // Ex01 ex01 = new Ex01();
+        // ex01.ex01();
+        
+        Ex02 ex02 = new Ex02();
+        ex02.ex02();
     }
 }
